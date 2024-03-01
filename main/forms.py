e58f92da49fc29dad186e2a6e2c9264685e063ba
lilliputten <lilliputten@yandex.ru>
2024-03-01 17:29:19 +0700

    Issue #22: Moved models and forms to their respective modules.

--- main/forms.py
+++ main/forms.py
@@ -1,5 +1,23 @@
-from .ApplicationForm import ApplicationForm
+from django.forms import ModelForm
+
+from .models import Application
+
+
+class ApplicationClientForm(ModelForm):
+    class Meta:
+        model = Application
+        exclude = ('id', 'secret_code', 'status', 'payment_status')
+        fields = '__all__'
+
+
+class ApplicationForm(ModelForm):
+    class Meta:
+        model = Application
+        exclude = ('id', 'secret_code')
+        fields = '__all__'
+
 
 __all__ = [
+    'ApplicationClientForm',
     'ApplicationForm'
 ]
--- main/models.py
+++ main/models.py
@@ -1,14 +1,72 @@
 from django.db import models
 from preferences.models import Preferences
+from django.db import models
 from django.conf import settings
-
-from .Application import Application
+import uuid
+from core.constants.date_time_formats import dateTimeFormat
 
 
 # See:
 # https://docs.djangoproject.com/en/5.0/intro/tutorial04/
 # https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Models
 # https://realpython.com/customize-django-admin-python/#modifying-a-change-list-using-list_display
+
+
+class Application(models.Model):
+
+    # Generated:
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    secret_code = models.UUIDField(default=uuid.uuid4)  # , editable=False)
+    #  secret_code = models.CharField(max_length=100)  # (generated)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # User data:
+    name = models.CharField(max_length=80, null=False, blank=False)
+    email = models.EmailField(max_length=80, null=False, blank=False)
+    text = models.CharField(max_length=200, blank=True)  # Comment
+
+    # Options:
+    PAYMENT_METHODS = (
+        ("STRIPE", "Stripe"),
+        ("INVOICE", "Invoice"),
+    )
+    payment_method = models.CharField(max_length=15, choices=PAYMENT_METHODS, default="STRIPE")  # (stripe, invoice)
+
+    # State:
+    STATUSES = (
+        ("WAITING", "Waiting"),
+        ("FINISHED", "Finished"),
+    )
+    status = models.CharField(max_length=15, choices=STATUSES, default="WAITING")  # (waiting, finished)
+    PAYMENT_STATUSES = (
+        ("WAITING", "Waiting"),
+        ("FINISHED", "Finished"),
+    )
+    payment_status = models.CharField(max_length=15, choices=PAYMENT_STATUSES, default="WAITING")  # (waiting, finished)
+    # TODO: Update neccessary status values
+
+    # Application options:
+    option_hackaton = models.BooleanField(default=False)  # pyright: ignore [reportArgumentType]
+    option_tshirt = models.BooleanField(default=False)  # pyright: ignore [reportArgumentType]
+
+    def __str__(self):
+        """
+        Human-readable representation of the object
+        """
+        info = ", ".join(list(filter(None, [
+            str(self.email) if self.email else None,
+            str(self.name) if self.name else None,
+            #  str(self.created_at) if self.created_at else "",
+            self.created_at.strftime(dateTimeFormat)  # pyright: ignore [reportAttributeAccessIssue]
+            if self.created_at else None,
+        ])))
+        info = " (" + info + ")" if info else None
+        info = " ".join(list(filter(None, [
+            str(self.id),
+            info,
+        ])))
+        return info
 
 
 class AllowedEmail(models.Model):
