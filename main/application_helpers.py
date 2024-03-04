f4b93654172abaa0da11bd4553eff32df11a8645
lilliputten <lilliputten@yandex.ru>
2024-03-04 23:58:05 +0700

    Issue #20: Added check for email in allowed emails list on application create/save. Minor changes (logs file names, removed custom log trace prefix).

--- main/application_helpers.py
+++ main/application_helpers.py
@@ -25,10 +25,9 @@ def pass_form_errors_to_messages(request: HttpRequest, form: ModelForm):
     if len(error_items):
         # TODO: Show errors?
         # Data example: {'name': ['This field is required.'], 'email': ['This field is required.']}
-        DEBUG(getTrace('Form has errors'), {
-            # 'error_items': error_items,  # NOTE: This dump causes TypeError: cannot pickle 'dict_items' object
-            # 'errors': errors,  # NOTE: This dump is huge (`!!python/object/new:django.forms.utils.ErrorDict`)
-        })
+        DEBUG(getTrace('Form has errors'))
+        # 'error_items': error_items,  # NOTE: This dump causes TypeError: cannot pickle 'dict_items' object
+        # 'errors': errors,  # NOTE: This dump is huge (`!!python/object/new:django.forms.utils.ErrorDict`)
         # Pass messages to client...
         for error, texts in error_items:  # pyright: ignore [reportOptionalMemberAccess]
             msg = capitalize_id(error) + ': ' + ' '.join(texts)
@@ -79,49 +78,39 @@ def get_and_update_application_from_request(request: HttpRequest, application_id
                 doSave = True
                 application = form.instance
                 cleaned_data = form.cleaned_data
-                # Get options list from post data...
+                # Get & update options list from post data...
                 new_options_ids = request.POST.getlist('options')
                 # Get suitable options for options' model QuerySet...
                 # @see: https://docs.djangoproject.com/en/5.0/topics/db/queries/
                 # pyright: ignore [reportAttributeAccessIssue]
-                new_options = EventOption.objects.filter(id__in=new_options_ids, active=True)
+                new_options = EventOption.objects.filter(id__in=new_options_ids, active=True)  # pyright: ignore [reportAttributeAccessIssue]
                 # Update many-to-many key (ManyRelatedManager) with a new options list...
                 application.options.set(new_options)
 
-                #  # TODO: Check email against allow_only_listed_emails and AllowedEmail
-                #  allow_only_listed_emails = \
-                #      preferences.SitePreferences.allow_only_listed_emails  # pyright: ignore [reportAttributeAccessIssue]
-                #  allowed_emails = AllowedEmail.objects.filter(  # pyright: ignore [reportAttributeAccessIssue]
-                #      allow_participation=True)  # pyright: ignore [reportAttributeAccessIssue]
+                # Get application's event...
+                event = application.event
+
+                # Check if current email is allowed...
+                allowed_emails_str = event.allowed_emails
+                allowed_emails = [s.strip(' ').lower() for s in allowed_emails_str.split(',')]
+                if len(allowed_emails):
+                    compare_email = cleaned_data['email'].lower()
+                    is_email_allowed = compare_email in allowed_emails
+                    if not is_email_allowed:
+                        doSave = False
+                        DEBUG(getTrace('get_and_update_application_from_request: Email is not allowed'), {
+                            'allowed_emails': allowed_emails,
+                            'compare_email': compare_email,
+                            'is_email_allowed': is_email_allowed,
+                        })
+                        messages.error(request, 'This email can not participate in the event, we are sorry.')
+
                 DEBUG(getTrace('get_and_update_application_from_request: Saving data'), {
+                    'allowed_emails': allowed_emails,
                     'application': application,
                     'cleaned_data': cleaned_data,
                     #  'allow_only_listed_emails': allow_only_listed_emails,
                 })
-                # TODO: Update logic to use `event.allowed_emails`
-                #  # If 'only allowed emails' mode has used...
-                #  if allow_only_listed_emails:
-                #      # Check if current email is in the list of available ones...
-                #      allowed_emails_list = list(map(lambda item: item.email, allowed_emails))
-                #      email = application.email
-                #      is_email_allowed = email in allowed_emails_list
-                #      DEBUG(getTrace('get_and_update_application_from_request: Check email...'), {
-                #          'allowed_emails_list': allowed_emails_list,
-                #          'email': email,
-                #          'is_email_allowed': is_email_allowed,
-                #          'allow_only_listed_emails': allow_only_listed_emails,
-                #          'allowed_emails': allowed_emails,
-                #      })
-                #      if not is_email_allowed:
-                #          doSave = False
-                #          DEBUG(getTrace('get_and_update_application_from_request: Email is not allowed'), {
-                #              'email': email,
-                #              'allowed_emails_list': allowed_emails_list,
-                #              'is_email_allowed': is_email_allowed,
-                #          })
-                #          messages.error(
-                #              request, 'This email cannot participate in the event, we are sorry.'.format(
-                #                  application.email, application.name))
                 if doSave:
                     application.save()
                     # TODO: Process the data in form.cleaned_data as required ... redirect to a new URL:
@@ -134,11 +123,6 @@ def get_and_update_application_from_request(request: HttpRequest, application_id
             # Create new application if absent...
             if not application:
                 application = Application()
-                #  # DEBUG: Provide some test data for a fresh application...
-                #  application.name = 'Test'  # pyright: ignore [reportAttributeAccessIssue]
-                #  application.option_hackaton = True
-                #  application.payment_method = 'INVOICE'
-                #  application.save()
             # Create form...
             form = ApplicationClientForm(instance=application)
         context = {
@@ -151,12 +135,9 @@ def get_and_update_application_from_request(request: HttpRequest, application_id
             options = application.options.all()  # pyright: ignore [reportAttributeAccessIssue]
             option_ids = list(map(lambda item: str(item.id), options))
             option_ids_joined = ','.join(option_ids)
-            options_count = len(options)
-            event_options = application.event.options.filter(
-                active=True)  # pyright: ignore [reportAttributeAccessIssue]
+            event_options = application.event.options.filter(  # pyright: ignore [reportAttributeAccessIssue]
+                active=True)
             posted_options = request.POST.getlist('options') if has_post_data and 'options' in request.POST else None
-            #  for option in options:
-            #      print(option['name'])
             DEBUG(getTrace('get_and_update_application_from_request: Result'), {
                 'option_ids': option_ids,
                 'options': options,
